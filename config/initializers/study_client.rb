66b892aebccd52d434b6d52fc9870ecf85512399
David Robinson <dr6@sanger.ac.uk>
2017-03-09 09:58:22 +0000

    json api clients - zipkin integrate on production and staging, not test and staging

--- config/initializers/study_client.rb
+++ config/initializers/study_client.rb
@@ -1,7 +1,7 @@
 Rails.application.config.after_initialize do
   StudyClient::Base.site = Rails.application.config.study_url
 
-  if Rails.env.test? || Rails.env.staging?
+  if Rails.env.production? || Rails.env.staging?
     StudyClient::Base.connection do |connection|
       connection.use ZipkinTracer::FaradayHandler
     end
