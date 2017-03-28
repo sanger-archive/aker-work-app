b1d5013da2e2b0791b087106b15de78c6d69f823
David Robinson <dr6@sanger.ac.uk>
2017-03-28 10:34:50 +0100

    Making the wizard back-and-forward proof
    1. Create the locked set before calculating the total cost.
    2. Don't ever try and create the locked set twice.
    3. Don't let the user change the selected set once the locked
       set has been created.
    4. Show total cost on the summary page.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
-
   include Wicked::Wizard
+
   steps :set, :product, :cost, :proposal, :summary
 
   def show
@@ -8,56 +8,23 @@ class OrdersController < ApplicationController
   end
 
   def update
-    unless params[:work_order].nil?
-      params[:work_order][:status] = step.to_s
-
-      if work_order.update_attributes(work_order_params) && last_step?
-        if work_order.product.suspended?
-          flash[:notice] = "That product is suspended and cannot currently be ordered."
-          render_wizard
-          return
-        end
-
-        begin
-          work_order.create_locked_set
-        rescue => e
-          logger.error "Failed to create locked set"
-          logger.error e.backtrace
-          flash[:error] = "The request to the set service failed."
-          render_wizard
-          return
-        end
-
-        begin
-          work_order.send_to_lims
-        rescue => e
-          logger.error "Failed to send work order"
-          logger.error e.backtrace
-          flash[:error] = "The request to the LIMS failed."
-          render_wizard
-          return
-        end
-
-        work_order.update_attributes(status: 'active')
-        flash[:notice] = 'Your Work Order has been created'
-      end
+    if params[:work_order].nil? || perform_step
+      render_wizard work_order
+    else
+      render_wizard
     end
-
-    render_wizard work_order
   end
 
-  protected
+protected
 
   def work_order
     @work_order ||= WorkOrder.find(params[:work_order_id])
   end
 
-  def aker_set
-    work_order.aker_set
-  end
-
   def get_all_aker_sets
-    SetClient::Set.all.select { |s| s.meta["size"] > 0 }
+    # Don't support altering the original set once the locked set has been created
+    return [work_order.original_set] unless work_order.set_uuid.nil?
+    return SetClient::Set.all.select { |s| s.meta["size"] > 0 }
   end
 
   def proposal
@@ -72,11 +39,6 @@ class OrdersController < ApplicationController
     Catalogue.where(current: true).all
   end
 
-  def get_set_size
-    return 0 if work_order.original_set_uuid.nil?
-    return work_order.original_set.meta['size']
-  end
-
   def last_step?
     step == steps.last
   end
@@ -85,9 +47,45 @@ class OrdersController < ApplicationController
     step == steps.first
   end
 
-  helper_method :work_order, :aker_set, :get_all_aker_sets, :item, :proposal, :get_all_proposals, :get_current_catalogues, :get_set_size, :item_option_selections, :last_step?, :first_step?
+  helper_method :work_order, :get_all_aker_sets, :proposal, :get_all_proposals, :get_current_catalogues, :item_option_selections, :last_step?, :first_step?
 
-  private
+private
+
+  def perform_step
+    params[:work_order][:status] = step.to_s
+
+    if work_order.original_set_uuid and not work_order.set_uuid
+      begin
+        work_order.create_locked_set
+      rescue => e
+        logger.error "Failed to create locked set"
+        logger.error e.backtrace
+        flash[:error] = "The request to the set service failed."
+        return false
+      end
+      logger.debug "Created locked set"
+    end
+
+    if work_order.update_attributes(work_order_params) && last_step?
+      if work_order.product.suspended?
+        flash[:notice] = "That product is suspended and cannot currently be ordered."
+        return false
+      end
+
+      begin
+        work_order.send_to_lims
+      rescue => e
+        logger.error "Failed to send work order"
+        logger.error e.backtrace
+        flash[:error] = "The request to the LIMS failed."
+        return false
+      end
+
+      work_order.update_attributes(status: 'active')
+      flash[:notice] = 'Your work order has been created'
+    end
+    return true
+  end
 
   def work_order_params
     params.require(:work_order).permit(
