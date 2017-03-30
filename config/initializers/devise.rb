17f8310a48c03ce9908eb99146f8e62596184829
Chris Smith <cs24@sanger.ac.uk>
2017-03-30 15:46:04 +0100

    Use fake ldap authenticatable thing for user testing

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,6 +271,10 @@ Devise.setup do |config|
   #   manager.intercept_401 = false
   #   manager.default_strategies(scope: :user).unshift :some_external_strategy
   # end
+  config.warden do |manager|
+    strategies = manager.default_strategies(:scope => :user)
+    strategies[strategies.index(:ldap_authenticatable)] = :fake_ldap_authenticatable
+  end
 
   # ==> Mountable engine configurations
   # When using Devise inside an engine, let's call it `MyEngine`, and this engine
@@ -285,4 +289,6 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+
+
 end
