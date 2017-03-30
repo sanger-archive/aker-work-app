0f6c29b3f575eea68d01d749002cc1eb3467d680
Chris Smith <cs24@sanger.ac.uk>
2017-03-30 12:07:38 +0100

    Use Bootstrap styles for login page. Added logout button

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,12 @@
 class Users::SessionsController < Devise::SessionsController
   after_action :store_session_data, only: [:create]
+  before_action :add_email_to_user, only: [:create]
 
   protected
+
+  def add_email_to_user
+    request.parameters["user"]["email"] += '@sanger.ac.uk' unless request.parameters["user"]["email"].include?('@')
+  end
 
   def store_session_data
     user_data = {
