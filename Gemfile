50434dac298b0f16bfa60d744e1957927bc90ff9
Harriet Craven <hc6@sanger.ac.uk>
2017-03-30 17:05:28 +0100

    Adds jwt encoding and tests to requests to Sets

--- Gemfile
+++ Gemfile
@@ -63,6 +63,11 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
+
+group :test do
+  gem 'webmock'
+end
+
 group :test, :development do
   gem 'rspec-rails', '~> 3.4'
   gem 'launchy'
--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,14 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe OrdersController, type: :controller do
+RSpec.describe ApplicationController, type: :controller do
 
 	describe "JWT Auth Header" do
 		context "when calling the set-service" do
 			it "sets the authorization header by using the session" do
-				work_order = create :work_order
-				patch(:update, {:work_order_id => work_order.id, :id => 'set'}, { principle_user: 'MyUserData'})
-				debugger
-				#expect(:get => new_widget_path).to route_to(:controller => "widgets", :action => "new")
+				principle_user = {:data => 'some_info'}
+				RequestStore.store[:x_authorisation] = principle_user
+				srequ = stub_request(:get, "#{Rails.configuration.set_url}/sets").
+         			with(:headers => {'Accept'=>'application/vnd.api+json',
+         				'Content-Type'=>'application/vnd.api+json',
+         				'X-Authorisation'=> JWTSerializer.generate_jwt(principle_user)}).
+         			to_return(:status => 200, :body => "", :headers => {})
+
+				SetClient::Set.all
+				assert_requested(srequ)
 			end
 		end
 
