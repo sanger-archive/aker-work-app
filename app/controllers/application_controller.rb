f3036ea6a1700e713cd081adee42c445d41f8401
Eduardo Martin Rojo <emr@sanger.ac.uk>
2017-05-15 16:06:43 +0100

    Revert for app_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,9 @@
 class ApplicationController < ActionController::Base
 
-  protect_from_forgery with: :exception
+  #protect_from_forgery with: :exception
 
-  include JWTCredentials
   include AkerAuthenticationGem::AuthController
+  include JWTCredentials
 
   include AkerPermissionControllerConfig
-
-  rescue_from StandardError do |e|
-    debugger
-  end
 end
