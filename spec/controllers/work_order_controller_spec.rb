a3ccf9063af23b2c2c037c70c8986d1047eed297
Harriet Craven <hc6@sanger.ac.uk>
2017-05-30 10:36:51 +0100

    added quick complete/cancel controller tests

--- spec/controllers/work_order_controller_spec.rb
+++ spec/controllers/work_order_controller_spec.rb
@@ -2,70 +2,80 @@ require 'rails_helper'
 
 RSpec.describe WorkOrdersController, type: :controller do
 	describe 'work_order' do
-		context 'completion' do
-			@msg = FactoryGirl.build(:work_order_completion_message_json)
-			@work_order = FactoryGirl.create :work_order
-			@msg[:work_order][:work_order_id] = @work_order.id
 
+    setup do
+      @request.env['devise.mapping'] = Devise.mappings[:user]
+      @msg = FactoryGirl.build(:work_order_completion_message_json)
+      @user = create(:user)
+      @wo = WorkOrder.create(user_id: @user.id, status: "active")
+
+      allow_any_instance_of(WorkOrderValidatorService).to receive(:validate?).and_return true
+
+      allow_any_instance_of(CreateNewMaterialsStep).to receive(:up).and_return true
+      allow_any_instance_of(CreateContainersStep).to receive(:up).and_return true
+      allow_any_instance_of(UpdateOldMaterialsStep).to receive(:up).and_return true
+      allow_any_instance_of(LockSetStep).to receive(:up).and_return true
+    end
+
+		context 'completion' do
 			it 'completes a work order' do
-				post complete_work_order_path(@work_order), params: @msg
-				expect(@work_order.status).to eq('complete')
+       	post :complete, work_order: @msg, id: @wo.id
+        @wo.reload
+				expect(@wo.status).to eq('completed')
 			end
 
 			it 'cancels a work order' do
-				post cancel_work_order_path(@work_order), params: @msg
-				expect(@work_order.status).to eq('cancel')
+				post :cancel, work_order: @msg, id: @wo.id
+        @wo.reload
+        expect(@wo.status).to eq('cancelled')
 			end
 		end
 
 		context '#new' do
 			it 'creates a work order with the logged on user' do
-		      @request.env['devise.mapping'] = Devise.mappings[:user]
 
-		      groups = ["cowboys"]
+        groups = ["cowboys"]
 
-		      user = create(:user)
-		      sign_in user
+        user = create(:user)
+        sign_in user
 
-		      expect { post :create, params: {} }.to change(WorkOrder, :count).by(1)
-	          expect(WorkOrder.last.user_id).to eq user.id
+        expect { post :create, params: {} }.to change(WorkOrder, :count).by(1)
+        expect(WorkOrder.last.user_id).to eq user.id
 
 			end
 		end
 
 		context "#index" do
 			it 'shows work orders belonging to the user' do
-				@request.env['devise.mapping'] = Devise.mappings[:user]
+	      groups = ["cowboys"]
 
-		      groups = ["cowboys"]
+	      user = create(:user)
+	      sign_in user
+	      user2 = create(:user, email: 'jeff@sanger.ac.uk')
+	      wo1 = WorkOrder.create(user_id: user.id, status: "product")
+	      wo2 = WorkOrder.create(user_id: user2.id, status: "product")
+	      wo3 = WorkOrder.create(user_id: user.id, status: WorkOrder.ACTIVE)
+	      wo4 = WorkOrder.create(user_id: user2.id, status: WorkOrder.ACTIVE)
+	      wo5 = WorkOrder.create(user_id: user.id, status: WorkOrder.COMPLETED)
+	      wo6 = WorkOrder.create(user_id: user2.id, status: WorkOrder.COMPLETED)
+		    wo7 = WorkOrder.create(user_id: user.id, status: WorkOrder.CANCELLED)
+	      wo8 = WorkOrder.create(user_id: user2.id, status: WorkOrder.CANCELLED)
 
-		      user = create(:user)
-		      sign_in user
-		      user2 = create(:user, email: 'jeff@sanger.ac.uk')
-		      wo1 = WorkOrder.create(user_id: user.id, status: "product")
-		      wo2 = WorkOrder.create(user_id: user2.id, status: "product")
-		      wo3 = WorkOrder.create(user_id: user.id, status: WorkOrder.ACTIVE)
-		      wo4 = WorkOrder.create(user_id: user2.id, status: WorkOrder.ACTIVE)
-		      wo5 = WorkOrder.create(user_id: user.id, status: WorkOrder.COMPLETED)
-		      wo6 = WorkOrder.create(user_id: user2.id, status: WorkOrder.COMPLETED)
-			    wo7 = WorkOrder.create(user_id: user.id, status: WorkOrder.CANCELLED)
-		      wo8 = WorkOrder.create(user_id: user2.id, status: WorkOrder.CANCELLED)
+	      get :index, params: {}
 
-		      get :index, params: {}
+        pending = controller.instance_variable_get("@pending_work_orders")
+	      expect(pending.length).to eq 1
+	      expect(pending.first).to eq wo1
 
-          pending = controller.instance_variable_get("@pending_work_orders")
-		      expect(pending.length).to eq 1
-		      expect(pending.first).to eq wo1
+        active = controller.instance_variable_get("@active_work_orders")
+	      expect(active.length).to eq 1
+	      expect(active.first).to eq wo3
 
-          active = controller.instance_variable_get("@active_work_orders")
-		      expect(active.length).to eq 1
-		      expect(active.first).to eq wo3
+        completed = controller.instance_variable_get("@completed_work_orders")
+	      expect(completed.length).to eq 1
 
-          completed = controller.instance_variable_get("@completed_work_orders")
-		      expect(completed.length).to eq 1
-
-          cancelled = controller.instance_variable_get("@cancelled_work_orders")
-		      expect(cancelled.length).to eq 1
+        cancelled = controller.instance_variable_get("@cancelled_work_orders")
+	      expect(cancelled.length).to eq 1
 			end
 		end
 	end
