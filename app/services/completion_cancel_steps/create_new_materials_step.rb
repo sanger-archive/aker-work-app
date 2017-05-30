caa53398aa3b7ea8123ac73c64b41bb680f23a1b
David Robinson <dr6@sanger.ac.uk>
2017-05-30 12:17:42 +0100

    create_new_materials_step
    Specify owner_id in POST to materials service (creating new material).
    Add material to container's slot instead of container itself
    (even if the container is a tube).

--- app/services/completion_cancel_steps/create_new_materials_step.rb
+++ app/services/completion_cancel_steps/create_new_materials_step.rb
@@ -25,25 +25,24 @@ class CreateNewMaterialsStep
     @msg[:work_order][:new_materials].each do |mat|
       container = mat[:container]
       mat.delete(:container)
-      # answer can either be an ResultSet or an array, if a ResultSet convert to an array
-      # add owner of materials?
-      answer = MatconClient::Material.create(mat)
+      mat[:owner_id] = @work_order.user.email
+      new_material = MatconClient::Material.create(mat)
+      # if result set or array, get the material from it
       if answer.class != MatconClient::Material
-        answer = answer.to_a
+        new_material = new_material.first
       end
-      new_material = [answer].flatten.first
 
       if container
         # Find the container and add the material to it
-        container_instance = get_container(container[:barcode]).to_a.first
+        container_instance = get_container(container[:barcode]).first
         raise ContainerNotFound unless container_instance
         @modified_container_before_save.push(container_instance)
-        # if container has key address it is a plate so add material to the address
+        # if container has key address it is (hopefully) a plate so add material to the address
         if container.has_key?(:address)
           container_instance.add_to_slot(container[:address], new_material)
         else
-        # container is a tube so directly add material
-          container_instance.material_id = new_material.id
+        # container is a tube so add to the tube's slot
+          container_instance.slots.first.material_id = new_material.id
         end
 
         # Add the containers_to_save to a list to save them afterwards
