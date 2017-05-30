207de4205da2d9a299312d00e3fd267b092cdda2
Eduardo Martin Rojo <emr@sanger.ac.uk>
2017-05-30 12:01:00 +0100

    Test for matcon json schema validate

--- spec/lib/work_order_completion_validator_spec.rb
+++ spec/lib/work_order_completion_validator_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
 require 'work_order_completion_validator'
+require 'support/test_services_helper'
 
 RSpec.describe 'WorkOrderCompletionValidator' do
+  include TestServicesHelper
   describe '#validate' do
+    setup do
+      webmock_matcon_schema
+    end
     it 'validates using a json schema from assets folder' do
-      expect(WorkOrderCompletionValidator.validate(FactoryGirl.build(:work_order_completion_message_json))).to eq(true)
+      expect(WorkOrderCompletionValidator.validate(FactoryGirl.build(:work_order_completion_message_json))).to eq([])
     end
   end
 end
--- spec/support/test_services_helper.rb
+++ spec/support/test_services_helper.rb
@@ -1,4 +1,31 @@
 module TestServicesHelper
+  
+  def webmock_containers_schema
+    @container_schema = %Q{
+      {"required": ["num_of_cols", "num_of_rows", "col_is_alpha", "row_is_alpha"], "type": "object", "properties": {"num_of_cols": {"max": 9999, "col_alpha_range": true, "required": true, "type": "integer", "min": 1}, "barcode": {"non_aker_barcode": true, "minlength": 6, "unique": true, "type": "string"}, "num_of_rows": {"row_alpha_range": true, "max": 9999, "required": true, "type": "integer", "min": 1}, "col_is_alpha": {"required": true, "type": "boolean"}, "print_count": {"max": 9999, "required": false, "type": "integer", "min": 0}, "row_is_alpha": {"required": true, "type": "boolean"}, "slots": {"uniqueaddresses": true, "type": "list", "schema": {"type": "dict", "schema": {"material": {"type": "uuid", "data_relation": {"field": "_id", "resource": "materials", "embeddable": true}}, "address": {"type": "string", "address": true}}}}}}
+    }
+
+    stub_request(:get, "http://localhost:5000/containers/json_schema").
+         to_return(status: 200, body: @container_schema, headers: {})    
+  end
+
+  def webmock_materials_schema
+    @material_schema = %Q{
+      {"required": ["gender", "donor_id", "phenotype", "supplier_name", "common_name"], "type": "object", "properties": {"gender": {"required": true, "type": "string", "enum": ["male", "female", "unknown"]}, "date_of_receipt": {"type": "string", "format": "date"}, "material_type": {"enum": ["blood", "dna"], "type": "string"}, "donor_id": {"required": true, "type": "string"}, "phenotype": {"required": true, "type": "string"}, "supplier_name": {"required": true, "type": "string"}, "common_name": {"required": true, "type": "string", "enum": ["Homo Sapiens", "Mouse"]}, "parents": {"type": "list", "schema": {"type": "uuid", "data_relation": {"field": "_id", "resource": "materials", "embeddable": true}}}, "owner_id": {"type": "string"}}}
+    }
+    stub_request(:get, "http://localhost:5000/materials/json_patch_schema").
+        to_return(status: 200, body: @material_schema, headers: {})
+
+    stub_request(:get, "http://localhost:5000/materials/json_schema").
+        to_return(status: 200, body: @material_schema, headers: {})
+
+  end
+
+  def webmock_matcon_schema
+    webmock_materials_schema
+    webmock_containers_schema
+  end
+
   def make_work_order
     @work_order = double(:work_order)
   end
