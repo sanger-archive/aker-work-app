7ee5293dfa29c720dab2e485ea367cea47db9c0d
Harriet Craven <hc6@sanger.ac.uk>
2017-06-02 16:08:37 +0100

    added product uuid

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,11 @@
 class Product < ApplicationRecord
   belongs_to :catalogue
 
+  after_initialize :create_uuid
+
   enum availability: { suspended: 0, available: 1 }
+
+  def create_uuid
+    self.product_uuid ||= SecureRandom.uuid
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170601150847) do
+ActiveRecord::Schema.define(version: 20170602144533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 20170601150847) do
     t.boolean  "r",               default: false, null: false
     t.boolean  "w",               default: false, null: false
     t.boolean  "x",               default: false, null: false
-    t.string   "accessible_type"
+    t.string   "accessible_type",                 null: false
     t.integer  "accessible_id",                   null: false
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20170601150847) do
     t.integer  "product_version"
     t.integer  "availability",                                       default: 1
     t.string   "description"
+    t.string   "product_uuid"
     t.index ["catalogue_id"], name: "index_products_on_catalogue_id", using: :btree
   end
 
