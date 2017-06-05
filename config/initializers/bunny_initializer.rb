a48239e39b92b90b41807fba9bbcaefd9a4d78d6
Eduardo Martin Rojo <emr@sanger.ac.uk>
2017-06-05 13:49:22 +0100

    Changed event publisher to confirm on every publish

--- config/initializers/bunny_initializer.rb
+++ config/initializers/bunny_initializer.rb
@@ -3,7 +3,8 @@ require 'event_publisher'
 require 'ostruct'
 
 if Rails.configuration.enable_events_sending
-  EventService = EventPublisher.new(event_conn: Rails.configuration.events_queue_connection, queue_name: Rails.configuration.events_queue_name)
+  EventService = EventPublisher.new(event_conn: Rails.configuration.events_queue_connection, 
+    queue_name: Rails.configuration.events_queue_name)
   # The connection should be created in the initializer, so we'll keep the following line
   # here (http://rubybunny.info/articles/connecting.html for more info)
   EventService.create_connection
--- lib/event_publisher.rb
+++ lib/event_publisher.rb
@@ -12,9 +12,13 @@ class EventPublisher
 
   def create_connection
     if !connected?
-      set_config
-      add_close_connection_handler
+      connect!
     end
+  end
+
+  def connect!
+    set_config
+    add_close_connection_handler    
   end
 
   def connected?
@@ -22,8 +26,14 @@ class EventPublisher
   end
 
   def publish(message)
-    create_connection if !connected?
-    @exchange.publish(message.generate_json, routing_key: @queue.name)
+    begin
+      create_connection if !connected?
+      @exchange.publish(message.generate_json, routing_key: @queue.name)
+      @channel.wait_for_confirms
+    rescue StandardError => e
+      puts 'An exception happened'
+      puts e.backtrace
+    end
   end
 
   def close
@@ -41,15 +51,21 @@ class EventPublisher
   end
 
   def set_config
-    @connection = Bunny.new(@event_conn)
+    # TODO
+    # threaded is set to false because otherwise the connection creation is not working
+    @connection = Bunny.new(@event_conn, threaded: false)
     @connection.start
 
     @channel = @connection.create_channel
+
+    #@channel.confirm_select
 
     @queue = channel.queue(@queue_name, :auto_delete => true)
 
     #@channel.confirm_select
     @exchange = @channel.default_exchange
+
+    @channel.confirm_select
   end
 
 
