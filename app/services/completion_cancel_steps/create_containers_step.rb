729f0e368789ac99b802dfc3aecab32252ad7f13
David Robinson <dr6@sanger.ac.uk>
2017-06-05 09:51:14 +0100

    fixed whitespace in create_containers_step.rb

--- app/services/completion_cancel_steps/create_containers_step.rb
+++ app/services/completion_cancel_steps/create_containers_step.rb
@@ -1,34 +1,34 @@
 class CreateContainersStep
-	attr_reader :containers
+  attr_reader :containers
 
-	def initialize(work_order, msg)
-		@work_order = work_order
-		@msg = msg
-	end
+  def initialize(work_order, msg)
+    @work_order = work_order
+    @msg = msg
+  end
 
-	def containers_to_create
-		@msg[:work_order][:containers].reject do |c| 
-				c.has_key?(:_id)
-		end.map do |c| 
-			c.merge({print_count: 0})
-		end
-	end
+  def containers_to_create
+    @msg[:work_order][:containers].reject do |c| 
+        c.has_key?(:_id)
+    end.map do |c| 
+      c.merge({print_count: 0})
+    end
+  end
 
-	# 1 - Create containers
-	def up
-		@containers = []
-		unless @msg[:work_order][:containers].empty?
-			elements = containers_to_create
+  # 1 - Create containers
+  def up
+    @containers = []
+    unless @msg[:work_order][:containers].empty?
+      elements = containers_to_create
 
-			unless elements.empty?
-	    	@containers = [MatconClient::Container.create(elements)].flatten
-	    end
-	  end
-	end
+      unless elements.empty?
+        @containers = [MatconClient::Container.create(elements)].flatten
+      end
+    end
+  end
 
-	def down
-		@containers.each do |c|
-			MatconClient::Container.destroy(c.id)
-		end
-	end
+  def down
+    @containers.each do |c|
+      MatconClient::Container.destroy(c.id)
+    end
+  end
 end
