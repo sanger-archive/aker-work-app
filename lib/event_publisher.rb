cb64954fb433efd0e8b46d928e4c8d8dca00743b
Eduardo Martin Rojo <emr@sanger.ac.uk>
2017-06-15 12:09:03 +0100

    Changes the exchange type to fanout

--- lib/event_publisher.rb
+++ lib/event_publisher.rb
@@ -4,6 +4,7 @@ class EventPublisher
 
   attr_accessor :connection
   attr_reader :channel, :exchange
+  attr_reader :dlx, :dlx_queue
 
   def initialize(config={})
     @event_conn = config[:event_conn]
@@ -53,11 +54,41 @@ class EventPublisher
     @connection = Bunny.new(@event_conn, threaded: false)
     @connection.start
 
+    dl_queue_name = @queue_name+'.deadletters'
+
+    @channel   = @connection.create_channel
+    @exchange    = @channel.fanout(@queue_name)
+    @dlx  = @channel.fanout(dl_queue_name)
+    @queue    = @channel.queue(@queue_name, :arguments => {
+      "x-dead-letter-exchange" => @dlx.name
+    }).bind(@exchange)
+    # dead letter queue
+    @dlx_queue  = @channel.queue(dl_queue_name).bind(@dlx)
+
+    # To be able to wait_for_confirms in publish()
+    @channel.confirm_select
+  end
+
+
+  def set_config2
+    # threaded is set to false because otherwise the connection creation is not working
+    @connection = Bunny.new(@event_conn, threaded: false)
+    @connection.start
+
     @channel = @connection.create_channel
+
+    # Creates the dead letter exchange aker.events.deadletters
+    @dlx = @channel.fanout(@queue_name+'.deadletters')
 
     # auto_delete false ensures that we dont destroy the queue when there are no messages and no 
     # consumers are running
-    @queue = channel.queue(@queue_name, :auto_delete => false)
+
+    # Creates the queue aker.events with dead letter exchange defined aker.events.deadletters
+    @queue = @channel.queue(@queue_name, :auto_delete => false, 
+      :arguments => {"x-dead-letter-exchange" => @queue_name+'.deadletters'})
+
+    # Creates the queue for the dead letters exchange
+    @dlx_queue = @channel.queue(@queue_name+'.deadletters').bind(@dlx)
 
     @exchange = @channel.default_exchange
 
