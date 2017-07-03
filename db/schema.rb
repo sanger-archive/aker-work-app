e69c96e2b769c7ebe8c6a71fd7e17860f892faaa
David Robinson <dr6@sanger.ac.uk>
2017-07-03 12:40:09 +0100

    Added remember_token to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170609092536) do
+ActiveRecord::Schema.define(version: 20170703111758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 20170609092536) do
     t.boolean  "r",               default: false, null: false
     t.boolean  "w",               default: false, null: false
     t.boolean  "x",               default: false, null: false
-    t.string   "accessible_type"
+    t.string   "accessible_type",                 null: false
     t.integer  "accessible_id",                   null: false
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20170609092536) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                       null: false
     t.datetime "updated_at",                       null: false
+    t.string   "remember_token"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
