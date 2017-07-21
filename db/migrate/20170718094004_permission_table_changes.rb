64af1d919a946cbead3aa5e616f44ff1ca7ec759
Harriet Craven <hc6@sanger.ac.uk>
2017-07-21 13:48:36 +0100

    Updated permission_table_changes migration

--- db/migrate/20170718094004_permission_table_changes.rb
+++ db/migrate/20170718094004_permission_table_changes.rb
@@ -1,23 +1,25 @@
 class PermissionTableChanges < ActiveRecord::Migration[5.0]
   def change
-  	add_column :permissions, :permission_type, :string, null: false
+    ActiveRecord::Base.transaction do |t|
+      add_column :permissions, :permission_type, :string, null: true
+      AkerPermissionGem::Permission.all.each do |p|
+        [
+          [:r, 'read'], [:w, 'write'], [:x, 'spend']
+        ].each do |column, new_value|
+          if p.send(column)
+            p.dup.update_attributes!(permission_type: new_value)
+          end
+        end
+        p.destroy
+      end
+    end
 
-  	ActiveRecord::Base.transaction do |t|
-  	  AkerPermissionGem::Permission.all.each do |p|
-  	  	[
-  	  	  [:r, 'read'], [:w, 'write'], [:x, 'spend']
-  	  	].each do |column, new_value|
-  	  	  if p.send(column)
-  	  	    p.dup.update_attributes!(permission_type: new_value)
-  	  	  end
-  	  	end
-  	  	p.destroy
-  	  end
-  	end
+    change_column :permissions, :permission_type, :string, null: false
+
+    remove_column :permissions, :r
+    remove_column :permissions, :w
+    remove_column :permissions, :x
+
     add_index :permissions, [:permitted, :permission_type, :accessible_id, :accessible_type ], unique: true, name: :index_permissions_on_various
-
-  	remove_column :permissions, :r
-  	remove_column :permissions, :w
-  	remove_column :permissions, :x
   end
 end
