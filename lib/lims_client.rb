9322895ed39f298ac08a89dd19063026290b2a15
Chris Smith <cs24@sanger.ac.uk>
2017-08-10 10:23:38 +0100

    Send Work Order to LIMS with Content Type as JSON

--- lib/lims_client.rb
+++ lib/lims_client.rb
@@ -3,15 +3,10 @@ require 'faraday'
 module LimsClient
 
   def self.post(url, params)
-    r = Faraday.new(url: url).post('', params.to_json)
+    r = Faraday.new(url: url, headers: { 'Content-Type': 'application/json' }).post('', params.to_json)
     unless r.status>=200 && r.status<400
     	raise "LimsClient post failed. Response status: #{r.status}"
     end
   end
 
-  def self.get_connection(url)
-    Faraday.new(:url => url) do |faraday|
-      faraday.use ZipkinTracer::FaradayHandler, 'work order service'
-    end
-  end
 end
