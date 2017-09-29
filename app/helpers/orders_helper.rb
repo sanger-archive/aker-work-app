cff03128e7b5186eaf1ed4872d6152a6e01230dd
Alex Cummins <alex.cummins@sanger.ac.uk>
2017-09-29 11:20:13 +0100

    Add new progress bar

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,21 @@
 module OrdersHelper
+  def step_classes(step)
+    steps = { "set" => 1, "proposal" => 2, "product" => 3, "cost" => 4, "summary" => 5 }
+    if params[:id] == "set" && step == 1
+      "active"
+    elsif params[:id] == "proposal" && step == 2
+      "active"
+    elsif params[:id] == "product" && step == 3
+      "active"
+    elsif params[:id] == "cost" && step == 4
+      "active"
+    elsif params[:id] == "summary" && step == 5
+      "active"
+    elsif step < steps[params[:id]]
+      "complete"
+    else
+      "upcoming"
+    end
+  end
+
 end
