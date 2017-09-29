3733d2f1c33d95309b6dd918a700e9c5d0d832d0
Alex Cummins <alex.cummins@sanger.ac.uk>
2017-09-29 11:46:13 +0100

    Improve if statement

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,15 +1,7 @@
 module OrdersHelper
   def step_classes(step)
     steps = { "set" => 1, "proposal" => 2, "product" => 3, "cost" => 4, "summary" => 5 }
-    if params[:id] == "set" && step == 1
-      "active"
-    elsif params[:id] == "proposal" && step == 2
-      "active"
-    elsif params[:id] == "product" && step == 3
-      "active"
-    elsif params[:id] == "cost" && step == 4
-      "active"
-    elsif params[:id] == "summary" && step == 5
+    if params[:id] == steps.key(step)
       "active"
     elsif step < steps[params[:id]]
       "complete"
