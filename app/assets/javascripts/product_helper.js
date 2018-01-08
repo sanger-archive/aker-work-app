f652987e406f133a4b481cadd43bd426e65ce829
Harriet Craven <hc6@sanger.ac.uk>
2018-01-08 16:57:57 +0000

    Playing around with product modules select elements

--- app/assets/javascripts/product_helper.js
+++ app/assets/javascripts/product_helper.js
@@ -19,6 +19,7 @@ $(document).on("turbolinks:load", function() {
       success: function(data){
         renderProductInformation(data);
         renderCostInformation(data);
+        renderDefaultProductDefinition(data);
       },
       error: function() {
         renderError('There was an error while accessing the Billing service');
@@ -27,9 +28,62 @@ $(document).on("turbolinks:load", function() {
   });
 });
 
+
 function renderError(msg) {
   var errorMsg = '<div class="alert-danger alert alert-dismissible" role="alert">'+msg+'</div>';
   $("#flash-display").html(errorMsg);
+}
+
+
+function renderDefaultProductDefinition(data) {
+
+  setProductData(data);
+  var updatedHTML = createElements();
+
+  $('#product-definition').html(updatedHTML);
+  $('#product-definition').show();
+
+  // on change of a select option
+  // get the element of the new selected
+  // go through the building select elements setting the new element as the selected
+
+  // $('#select0').change(function(){
+  //   let newValue = $('#select0').val();
+  //   let html = createElements(newValue);
+  //   debugger
+  //   updatedHTML = html + createSelect(availableLinks[newValue], newValue);
+
+  //   $('#product-definition').html(updatedHTML);
+  //   $('#product-definition').show();
+  // });
+
+
+  function createElements() {
+    var selectHtml = '';
+    for (let i = 0; i < defaultPath.length; ++i) {
+      let defaultKey;
+      i == 0 ? defaultKey = 'start' : defaultKey = defaultPath[i-1];
+      selectHtml += createSelect(availableLinks[defaultKey], defaultPath[i], i);
+    }
+    return selectHtml;
+  }
+
+  function createSelect(options, defaultOption, selectNumber) {
+    let selectString = `<select style="width:100px" id="select${selectNumber}">`;
+    if (typeof(options) == 'string') {
+      selectString += "<option>" + options + "</option>";
+    } else {
+      for (let i = 0; i < options.length; ++i) {
+        if (defaultOption == options[i]) {
+          selectString += "<option selected>" + options[i] + "</option>";
+        } else {
+          selectString += "<option>" + options[i] + "</option>";
+        }
+      }
+    }
+    selectString += '</select>';
+    return selectString;
+  }
 }
 
 function renderProductInformation(data) {
@@ -70,3 +124,35 @@ function convertToCurrency(input) {
   }
   return '£' + input.toFixed(2);
 };
+
+function setProductData(data) {
+  if (data.name == 'Cancer cell line banking test'){
+    defaultPath = ['X','L','O'];
+    availableLinks = {
+      'start': ['A','X','B'],
+      'A': 'end',
+      'B': 'end',
+      'O':'end',
+      'L': ['O','end'],
+      'X': ['L', 'O', 'end']
+    }
+  } else if (data.name == "CRISPR Guide RNA transduction"){
+    defaultPath = ['D','C'];
+    availableLinks = {
+      'start': ['A','B','C','D'],
+      'A': 'end',
+      'B': 'end',
+      'C':'end',
+      'D': ['A','B','C'],
+    }
+  } else {
+    defaultPath = ['id1','id4', 'id7'];
+    availableLinks = {
+      'start': ['id1','id3','id8'],
+      'id7': 'end',
+      'id5': 'end',
+      'id4': ['id7','end'],
+      'id1': ['id5','id4'],
+    }
+  }
+}
