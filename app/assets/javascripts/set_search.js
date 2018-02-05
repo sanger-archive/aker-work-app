6e2fbb2b27aeee7f6fcf3291bbe55747bae6bb87
Alex Cummins <alex.cummins@sanger.ac.uk>
2018-02-05 10:27:08 +0000

    Use own timeSince method

--- app/assets/javascripts/set_search.js
+++ app/assets/javascripts/set_search.js
@@ -51,29 +51,21 @@ function setSearch() {
   });
 }
 
-// Replicates Rails time_ago_in_words, courtesy of https://stackoverflow.com/a/3177838
+// Roughly emulates Rails time_ago_in_words
 function timeSince(date) {
-  var seconds = Math.floor((new Date() - new Date(date)) / 1000);
-  var interval = Math.floor(seconds / 31536000);
+  var timeSince = Math.floor((new Date() - new Date(date)) / 1000);
 
-  if (interval > 1) {
-    return interval + " years";
+  if (timeSince >= 31557600) {
+    return `${Math.round(timeSince / 31557600)} years`
+  } else if (timeSince >= 2592000) {
+    return `${Math.round(timeSince / 2592000)} months`
+  } else if (timeSince >= 86400) {
+    return `${Math.round(timeSince / 86400)} days`
+  } else if (timeSince >= 3600) {
+    return `${Math.round(timeSince / 3600)} hours`
+  } else if (timeSince >= 60) {
+    return `${Math.round(timeSince / 60)} minutes`
+  } else {
+    return `${Math.floor(timeSince)} seconds`
   }
-  interval = Math.floor(seconds / 2592000);
-  if (interval > 1) {
-    return interval + " months";
-  }
-  interval = Math.floor(seconds / 86400);
-  if (interval > 1) {
-    return interval + " days";
-  }
-  interval = Math.floor(seconds / 3600);
-  if (interval > 1) {
-    return interval + " hours";
-  }
-  interval = Math.floor(seconds / 60);
-  if (interval > 1) {
-    return interval + " minutes";
-  }
-  return Math.floor(seconds) + " seconds";
 }
