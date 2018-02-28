0ff7af7136c1517272d9c1a6e37fd3d41d8913be
Harriet Craven <hc6@sanger.ac.uk>
2018-02-28 09:52:52 +0000

    Fixed product migrations so they are reversible

--- db/migrate/20180222121021_rename_product_column_name.rb
+++ db/migrate/20180222121021_rename_product_column_name.rb
@@ -1,5 +1,12 @@
 class RenameProductColumnName < ActiveRecord::Migration[5.1]
   def change
-    rename_column :products, :available, :availability
+    reversible do |dir|
+      dir.up do
+        rename_column :products, :available, :availability
+      end
+      dir.down do
+        rename_column :products, :availability, :available
+      end
+    end
   end
 end
