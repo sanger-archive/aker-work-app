14c8555df8ad834727bd0b9a5e1d66d2cec946c0
David Robinson <14000840+khelwood@users.noreply.github.com>
2018-03-09 16:08:04 +0000

    simplified show_product_inside_work_plan

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,37 +8,27 @@ class ProductsController < ApplicationController
     cost_code = @work_plan.project.cost_code
     price = BillingFacadeClient.get_unit_price(cost_code, @product.name)
 
-    processes = []
-
-    @product.processes.each do |process|
-      process_info = {}
-
-      available_links = process.build_available_links
-      path = process.build_default_path
-
-      # Replace the default path by the plan's currently selected module path, if such a thing exists
-      if @work_plan.product_id==@product.id
-        order = @work_plan.work_orders.where(process_id: process.id).first
-        if order
-          path = order.selected_path
-        end
-      end
-
-      process_info[:name] = process.name
-      process_info[:id] = process.id
-      process_info[:links] = available_links
-      process_info[:path] = path
-
-      processes.push(process_info)
+    processes = @product.processes.map do |process|
+      { name: process.name, id: process.id, links: process.build_available_links, path: selected_modules(process) }
     end
 
     render json: @product.as_json.merge(
       unit_price: price, cost_code: cost_code, product_processes: processes
     ).to_json
-
   end
 
-  private
+private
+
+  # Returns the selected modules if such things exist; otherwise the default path modules
+  def selected_modules(process)
+    if @work_plan.product_id==@product.id
+      order = @work_plan.work_orders.where(process_id: process.id).first
+      if order
+        return order.selected_path
+      end
+    end
+    process.build_default_path
+  end
 
   def set_work_plan
     @work_plan = WorkPlan.find(params[:id])
