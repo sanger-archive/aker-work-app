d6e60902c314c902dc0bff1fac84ee07d1a1c5c4
Alex <NikolausC@users.noreply.github.com>
2018-03-20 13:59:07 +0000

    Update set shaper URL

--- app/helpers/plan_wizard_helper.rb
+++ app/helpers/plan_wizard_helper.rb
@@ -1,7 +1,7 @@
 module PlanWizardHelper
   def view_set_link(set)
     return unless set
-    url = Rails.application.config.set_shaper_url+'/simple/sets/'+set.uuid
+    url = Rails.configuration.urls[:sets] + '/simple/sets/' + set.uuid
     link_to(set.name, url, target: :_blank)
   end
 end
