eb4e7e76b5100ff239defc10383cb139d952932b
Harriet Craven <hc6@sanger.ac.uk>
2018-03-28 15:52:52 +0100

    Migration for creating jobs table, and model

---
+++ app/models/job.rb
@@ -0,0 +1,26 @@
+class Job < ApplicationRecord
+  belongs_to :work_order
+
+  def queued?
+    status == 'queued'
+  end
+
+  def active?
+    status == 'active'
+  end
+
+  def cancelled?
+    status == 'cancelled'
+  end
+
+  def completed?
+    status == 'completed'
+  end
+
+  def status
+    return 'cancelled' if cancelled
+    return 'queued' if [started, cancelled, complete].all?(&:nil?)
+    return 'active' if !started.nil? && [cancelled, completed].all?(&:nil?)
+    return 'completed' if !completed.nil? && [started, cancelled].all?(&:nil?)
+  end
+end
---
+++ db/migrate/20180328142742_create_jobs.rb
@@ -0,0 +1,11 @@
+class CreateJobs < ActiveRecord::Migration[5.1]
+  def change
+    create_table :jobs, id: :serial do |t|
+      t.uuid :container_uuid
+      t.datetime :started
+      t.datetime :completed
+      t.datetime :cancelled
+      t.references :work_order, foreign_key: true, null: false
+    end
+  end
+end
