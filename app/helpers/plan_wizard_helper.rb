53f229af1d3f500f2ead0c5785a095f4ac366944
Alex Cummins <alex.cummins@sanger.ac.uk>
2018-07-19 09:44:28 +0100

    Show set size on Work Plan dispatch page

--- app/helpers/plan_wizard_helper.rb
+++ app/helpers/plan_wizard_helper.rb
@@ -4,4 +4,8 @@ module PlanWizardHelper
     url = Rails.configuration.urls[:sets] + '/simple/sets/' + set.uuid
     link_to(set.name, url, target: :_blank)
   end
+
+  def view_set_link_with_size(set)
+    view_set_link(set) + " [#{set.meta["size"]} samples]"
+  end
 end
