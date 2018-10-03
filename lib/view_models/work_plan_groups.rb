62cddca7c6d0e5c6a906656eb64349fb7a361771
Harriet Craven <hc6@sanger.ac.uk>
2018-10-03 13:54:50 +0100

    code climate fixes in work plan groups

--- lib/view_models/work_plan_groups.rb
+++ lib/view_models/work_plan_groups.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
+# A helper class for work plans #index view, grouping plans by status
 module ViewModels
   class WorkPlanGroups
     attr_reader :work_plans
 
-    def initialize(args={})
+    def initialize(args = {})
       @work_plans = args.fetch(:work_plans, WorkPlan.all)
     end
 
@@ -11,31 +14,31 @@ module ViewModels
     end
 
     def active
-      grouped_work_plans['active']|| []
+      grouped_work_plans['active'] || []
     end
 
     def closed
-      grouped_work_plans['closed']|| []
+      grouped_work_plans['closed'] || []
     end
 
     def cancelled
-      grouped_work_plans['cancelled']|| []
+      grouped_work_plans['cancelled'] || []
     end
 
     def any_in_construction?
-      in_construction.length > 0
+      in_construction.length.positive?
     end
 
     def any_active?
-      active.length > 0
+      active.length.positive?
     end
 
     def any_closed?
-      closed.length > 0
+      closed.length.positive?
     end
 
     def any_cancelled?
-      cancelled.length > 0
+      cancelled.length.positive?
     end
 
     private
@@ -43,6 +46,5 @@ module ViewModels
     def grouped_work_plans
       @grouped_work_plans ||= work_plans.order(updated_at: :desc).group_by(&:status)
     end
-
   end
 end
