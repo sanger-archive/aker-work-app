3cb99c4faf68c27e7c522760a3be448840648f5a
Eduardo Martin Rojo <emr@sanger.ac.uk>
2018-10-11 10:33:54 +0100

    Billing facade initializer changes

--- config/initializers/billing_facade_client.rb
+++ config/initializers/billing_facade_client.rb
@@ -1,18 +1,16 @@
-unless Rails.env.test?
-  require 'billing_facade_client'
+require 'billing_facade_client'
 
-  Rails.application.config.after_initialize do
-    BillingFacadeClient.site = Rails.application.config.billing_facade_url
-    BillingFacadeClient.ubw_site = Rails.application.config.ubw_service_url
+Rails.application.config.after_initialize do
+  BillingFacadeClient.site = Rails.application.config.billing_facade_url
+  BillingFacadeClient.ubw_site = Rails.application.config.ubw_service_url
 
-    BillingFacadeClient.connection do |connection|
-      ENV['HTTP_PROXY'] = nil
-      ENV['http_proxy'] = nil
-      ENV['https_proxy'] = nil
-      connection.faraday.proxy {}
-      if Rails.env.production? || Rails.env.staging?
-        connection.use ZipkinTracer::FaradayHandler, 'BillingFacade service'
-      end
+  BillingFacadeClient.connection do |connection|
+    ENV['HTTP_PROXY'] = nil
+    ENV['http_proxy'] = nil
+    ENV['https_proxy'] = nil
+    connection.faraday.proxy {}
+    if Rails.env.production? || Rails.env.staging?
+      connection.use ZipkinTracer::FaradayHandler, 'BillingFacade service'
     end
   end
 end
