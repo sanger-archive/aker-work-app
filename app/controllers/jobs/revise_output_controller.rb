987fc997c0d0a4f82826a2cb13f3f0b0d4262001
Chris Smith <cs24@sanger.ac.uk>
2019-01-16 17:03:19 +0000

    Attempted some Code Climate fixes

--- app/controllers/jobs/revise_output_controller.rb
+++ app/controllers/jobs/revise_output_controller.rb
@@ -6,11 +6,7 @@ class Jobs::ReviseOutputController < ApplicationController
     success, value = create_revised_set
 
     respond_to do |format|
-      if success
-        format.js { render status: :created, template: 'jobs/_set_link_with_size', locals: { set: value } }
-      else
-        format.js { render json: { error: value }, status: :unprocessable_entity }
-      end
+      format.js { render js_render_options(success, value) }
     end
   end
 
@@ -25,21 +21,25 @@ private
   end
 
   def create_revised_set
-    if job.revised_output_set_uuid
-      return false, "This job already has a revised output set."
-    elsif job.forwarded
-      return false, "This job has already been forwarded to the next process."
-    elsif !job.output_set_uuid
-      return false, "This job does not yet have an output set."
+    return false, "This job already has a revised output set." if job.revised_output_set_uuid
+    return false, "This job has already been forwarded to the next process." if job.forwarded
+    return false, "This job does not yet have an output set." if !job.output_set_uuid
+
+    begin
+      return true, job.create_editable_set
+    rescue => e
+      Rails.logger.error "create_revised_set failed for job #{job.id}"
+      Rails.logger.error e
+      e.backtrace.each { |x| Rails.logger.error x }
+      return false, "The new set could not be created."
+    end
+  end
+
+  def js_render_options(success, value)
+    if success
+      { status: :created, template: 'jobs/_set_link_with_size', locals: { set: value } }
     else
-      begin
-        return true, job.create_editable_set
-      rescue => e
-        Rails.logger.error "create_revised_set failed for job #{job.id}"
-        Rails.logger.error e
-        e.backtrace.each { |x| Rails.logger.error x }
-        return false, "The new set could not be created."
-      end
+      { status: :unprocessable_entity, json: { error: value } }
     end
   end
 
